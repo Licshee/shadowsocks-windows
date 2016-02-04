045cd3b363802b303d5bf636f0236dc2589198ad
Licshee <licshee@yen.ai>
2016-02-04 15:05:34 +0800

    *sigh*

--- shadowsocks-csharp/Controller/I18N.cs
+++ shadowsocks-csharp/Controller/I18N.cs
@@ -1,31 +1,33 @@
-﻿using Shadowsocks.Properties;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Text;
-using System.Text.RegularExpressions;
+using System.Globalization;
+using System.IO;
 
 namespace Shadowsocks.Controller
 {
+    using Shadowsocks.Properties;
+
     public class I18N
     {
         protected static Dictionary<string, string> Strings;
         static I18N()
         {
-            Strings = new Dictionary<string, string>();
+            Strings = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
 
-            if (System.Globalization.CultureInfo.CurrentCulture.IetfLanguageTag.ToLowerInvariant().StartsWith("zh"))
+            if (CultureInfo.CurrentCulture.IetfLanguageTag.StartsWith("zh"))
             {
-                string[] lines = Regex.Split(Resources.cn, "\r\n|\r|\n");
-                foreach (string line in lines)
+                using (var sr = new StringReader(Resources.cn))
                 {
-                    if (line.StartsWith("#"))
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
                     {
-                        continue;
-                    }
-                    string[] kv = Regex.Split(line, "=");
-                    if (kv.Length == 2)
-                    {
-                        Strings[kv[0]] = kv[1];
+                        if (line[0] == '#')
+                            continue;
+
+                        var pos = line.IndexOf('=');
+                        if (pos < 1)
+                            continue;
+                        Strings[line.Substring(0, pos)] = line.Substring(pos + 1);
                     }
                 }
             }
